7e1a852cd533b4c5f3633266a323ab67406465cf
Phong <thanhphong102006@gmail.com>
2026-06-23 08:30:12 +0700

    update

---
+++ src/main/java/com/example/it211_session19_bt1/It211Session19Bt1Application.java
@@ -0,0 +1,11 @@
+package com.example.it211_session19_bt1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It211Session19Bt1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(It211Session19Bt1Application.class, args);
+    }
+}
